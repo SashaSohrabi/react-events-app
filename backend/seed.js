dbc98033a18f18517c677778239a179db8d00ea7
Malick <219251831+yaaintmal@users.noreply.github.com>
2025-10-11 13:08:15 +0200

    added image property to events-model (#2)

--- backend/seed.js
+++ backend/seed.js
@@ -30,6 +30,8 @@ const seedDB = async () => {
       latitude: 40.785091,
       longitude: -73.968285,
       organizerId: 1,
+      image:
+        "https://cityparksfoundation.org/wp-content/uploads/2025/04/6.15.24_SummerStage_AussieBBQ_CentralPark_MerissaBlitz298-scaled-1-1.webp",
     },
     {
       title: "Tech Conference",
@@ -39,6 +41,8 @@ const seedDB = async () => {
       latitude: 37.774929,
       longitude: -122.419418,
       organizerId: 2,
+      image:
+        "https://skift.com/wp-content/uploads/2014/04/SwissTechCC-Lausanne.jpg",
     },
     {
       title: "Oktoberfest",
@@ -49,6 +53,8 @@ const seedDB = async () => {
       latitude: 48.131271,
       longitude: 11.549669,
       organizerId: 1,
+      image:
+        "https://rp-online.de/imgs/32/2/4/3/8/1/2/1/1/9/tok_ed203074f96d695e31cfc6e8aefc8a72/w2048_h1203_x1024_y601_DPA_dpa_urn_newsml_dpa.com_20090101_250920-911-006531-v4-s2048-837e47b13b837355.jpeg",
     },
     {
       title: "Berlin Marathon",
@@ -59,6 +65,8 @@ const seedDB = async () => {
       latitude: 52.516275,
       longitude: 13.377704,
       organizerId: 3,
+      image:
+        "https://norqain.it/cdn/shop/files/Partnership_Hero_BMW_Marathon.jpg?v=1692815357&width=2500",
     },
     {
       title: "Christmas Market",
@@ -69,6 +77,8 @@ const seedDB = async () => {
       latitude: 52.521918,
       longitude: 13.413215,
       organizerId: 3,
+      image:
+        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/8a/7d/25/caption.jpg?w=1200&h=-1&s=1",
     },
   ];
 
