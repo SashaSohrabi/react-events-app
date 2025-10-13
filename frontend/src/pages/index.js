a87c805377684a5cb482e4dc19fb61ebfbaa112a
yaaintmal <me@problack.de>
2025-10-13 04:36:24 +0200

    refactored

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,8 +3,6 @@ import Events from './Events';
 import EventDetail from './EventDetail';
 import NotFound from './NotFound';
 import NewEvent from './NewEvent';
-import Events from './Events';
-import EventDetail from './EventDetail';
 import Auth from './Auth';
 
 export { Home, NotFound, NewEvent, Auth, Events, EventDetail };
