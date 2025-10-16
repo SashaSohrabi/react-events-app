883c7e9c1a89ad58b50283c1929cb00df2cdc821
sashasohrabi <shb.sohrabi@gmail.com>
2025-10-16 16:59:00 +0200

    chore: load API base url from Vite env files

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,7 +1,8 @@
 export const DEFAULT_THEME = 'light';
 export const ALT_THEME = 'dark';
 export const AUTH_STORAGE_KEY = 'authUser';
-export const BASE_URL = 'http://localhost:3001';
+export const BASE_URL =
+  import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:3001';
 
 export const LEGENDS = {
   login: 'Login',
